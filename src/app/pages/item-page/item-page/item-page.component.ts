6708f4f609b42f4c14db851420649884bd671725
Alejandro Maldonado <91658941+reychu@users.noreply.github.com>
2023-01-15 16:15:56 +0100

    Design ItemPageComponent

--- src/app/pages/item-page/item-page/item-page.component.ts
+++ src/app/pages/item-page/item-page/item-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Item } from 'src/app/items/item';
+import { ItemService } from 'src/app/items/item.service';
 
 @Component({
   selector: 'app-item-page',
@@ -8,10 +11,46 @@ import { Router } from '@angular/router';
 })
 export class ItemPageComponent implements OnInit {
 
-  constructor(private router:Router) { }
+  item:Item;
+
+  constructor(private router: Router, private itemService:ItemService) { }
 
   ngOnInit(): void {
     console.log(this.router.url);
+    this.getItemById();
   }
 
+  getItemById(){
+    var id = /[^/]*$/.exec(this.router.url)[0];
+    console.log(id);
+    this.getByItemId(id).subscribe(res => {
+      this.item = res;
+    })
+  }
+
+  getByItemId(id): Observable<Item>{
+    return this.itemService.getItemById(id);
+  }
+
+  nameFunction(item) {
+    //console.log("hit");
+    let name = item.itemName;
+    if (name.includes("ü")) {
+      name = name.replace("ü", "ue");
+    } else if (name.includes("ä")) {
+      name = name.replace("ä", "ae");
+    } else if (name.includes("ö")) {
+      name = name.replace("ö", "oe");
+    }
+
+    return name;
+
+  }
+
+  getDate(){
+    const dateInMil = this.item.date;
+    console.log(dateInMil);
+    const date = (new Date(dateInMil)).toLocaleDateString();
+    return date;
+  }
 }
