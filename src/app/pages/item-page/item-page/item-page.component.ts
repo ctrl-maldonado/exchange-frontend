a722491b87df35db39e76ec019bf52e457d3d12d
Alejandro Maldonado <91658941+reychu@users.noreply.github.com>
2023-01-15 13:58:42 +0100

    Added ItemPage Component

---
+++ src/app/pages/item-page/item-page/item-page.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-item-page',
+  templateUrl: './item-page.component.html',
+  styleUrls: ['./item-page.component.css']
+})
+export class ItemPageComponent implements OnInit {
+
+  constructor(private router:Router) { }
+
+  ngOnInit(): void {
+    console.log(this.router.url);
+  }
+
+}
