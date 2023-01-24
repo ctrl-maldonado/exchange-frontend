553b0bf949137ab0e4310e8df26dee6618725710
Alejandro Maldonado <91658941+reychu@users.noreply.github.com>
2023-01-24 16:34:38 +0100

    Validierung fehlt noch

--- src/app/pages/login/login/login.component.ts
+++ src/app/pages/login/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, ErrorHandler, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
@@ -16,7 +16,8 @@ export class LoginComponent implements OnInit {
 
   constructor(private router: Router,
     private authService: AuthService,
-    private fb: FormBuilder) {
+    private fb: FormBuilder,
+    provide: ErrorHandler) {
     this.form = this.fb.group({
       email: ['', Validators.required],
       password: ['', Validators.required]
@@ -30,22 +31,19 @@ export class LoginComponent implements OnInit {
     const val = this.form.value;
 
     if (val.email && val.password) {
-      this.authService.login(val.email, val.password)
+      try {
+        this.authService.login(val.email, val.password)
         .subscribe(
           (user) => {
-            //this.authService.loggedInUser = user;
-            /*const password = user.password;
-            console.log(password);
-            var encrypted = CryptoJS.AES.encrypt(password, "cheers");
-            var decrypted = CryptoJS.AES.decrypt(encrypted, "cheers");
-            console.log(encrypted.toString(CryptoJS.enc.Utf8));
-            console.log(decrypted.toString(CryptoJS.enc.Utf8));*/
             delete user.password;
             window.sessionStorage.setItem('loggedInUser', JSON.stringify(user));
             console.log("User is logged in");
             this.router.navigateByUrl('/');
           }
         );
+      } catch (err) {
+        alert(err)
+      }
     }
   }
 }
